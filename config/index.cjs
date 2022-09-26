0b140272ca49238c4dac99b910347362aad37bb5
Zhangbrqs <87114782+Zhangbrqs@users.noreply.github.com>
2022-09-26 22:42:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -7,12 +7,12 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: 'wxfc0cce5d336d39ab',
+  APP_ID: '',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: 'b681c6cc5bbd18d89cf960bbea1fa255',
+  APP_SECRET: '',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -65,9 +65,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: 'hello',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oV4Sd56wtTjJS9KqxDv2MEOgZgSc',
+      id: '',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: 'xZTiWMJImQZuSYsMsElED6iHHKE9jT8pq-wt5UESdWU',
+      useTemplateId: '',
       // 所在省份
       province: '广东',
       // 所在城市
